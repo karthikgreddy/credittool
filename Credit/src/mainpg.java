e857b79c9b509b7b0abbce9cd8924dcab566d154
karthik reddy <karthik reddy@10.9.94.119>
2023-12-05 10:22:13 +0000

    Imported Excel

--- Credit/src/mainpg.java
+++ Credit/src/mainpg.java
@@ -6,11 +6,18 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
 
 public class mainpg extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -44,13 +51,25 @@ public class mainpg extends JFrame {
 		JLabel lblNewLabel = new JLabel("Check Your Credit Score :");
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel.setBounds(29, 25, 251, 37);
+		lblNewLabel.setBounds(21, 31, 251, 37);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Check Your Credit Score :");
-		lblNewLabel_1.setForeground(Color.WHITE);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_1.setBounds(484, 325, 251, 37);
-		contentPane.add(lblNewLabel_1);
+		JButton btnUpload = new JButton("UPLOAD");
+		btnUpload.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser jf = new JFileChooser();
+				int result = jf.showOpenDialog(null);
+			}
+		});
+		btnUpload.setForeground(Color.WHITE);
+		btnUpload.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnUpload.setBackground(new Color(46, 41, 79));
+		btnUpload.setBounds(66, 603, 225, 41);
+		contentPane.add(btnUpload);
+		
+		table = new JTable();
+		table.setBackground(new Color(46, 48, 79));
+		table.setBounds(66, 109, 1214, 456);
+		contentPane.add(table);
 	}
 }
